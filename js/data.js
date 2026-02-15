8b1fb4b07f81b8306726285bf6d96bb199f23806
aburayya <132553021+aburayya@users.noreply.github.com>
2026-02-16 00:56:29 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,4 +1,7 @@
 async function loadWords() {
+  const local = localStorage.getItem("nawa_words");
+  if (local) return JSON.parse(local);
+
   const res = await fetch("data/words.json");
   return await res.json();
 }
